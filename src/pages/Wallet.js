959316bc0b9fe4607b7a62f32fdae11822d40e12
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-03-09 15:31:21 -0300

    Requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,43 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formDespesa: {
+        id: 0,
+        valorDespesa: 0,
+        descricaoDespesa: '',
+        moeda: 'BRL',
+        metodoPagamento: 'Dinheiro',
+        categoriaDespesa: 'Transporte',
+      },
+      despesasTot: [],
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState((prevState) => ({
+      formDespesa: {
+        ...prevState.formDespesa,
+        [name]: value,
+      },
+    }));
+  }
+
+  handleButton = () => {
+    const { formDespesa, despesasTot } = this.state;
+    const { id } = formDespesa;
+    // if (despesasTot.length > 0) {
+    //   this.setState({
+    //     formDespesa[id]: id + 1 ,
+    //   });
+    // }
+    this.setState({
+      despesasTot: [...despesasTot, formDespesa],
+    });
+  }
+
   render() {
     const { email } = this.props;
     return (
@@ -13,6 +50,76 @@ class Wallet extends React.Component {
           <p data-testid="total-field">0</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+        <form>
+          <label htmlFor="valorDespesa">
+            Valor da Despesa:
+            <input
+              name="valorDespesa"
+              type="number"
+              id="valorDespesa"
+              data-testid="value-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="descricaoDespesa">
+            Descrição da Despesa:
+            <input
+              id="descricaoDespesa"
+              name="descricaoDespesa"
+              type="text"
+              data-testid="description-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select
+              id="currency"
+              name="moeda"
+              type="select"
+              data-testid="currency-input"
+              onChange={ this.handleChange }
+            >
+              <option value="Moeda">Moeda</option>
+            </select>
+          </label>
+          <label htmlFor="metodoPagamento">
+            Método de Pagamento:
+            <select
+              id="metodoPagamento"
+              name="metodoPagamento"
+              type="select"
+              data-testid="method-input"
+              onChange={ this.handleChange }
+            >
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartaoCredito">Cartão de Crédito</option>
+              <option value="cartaoDebito">Cartão de Débito</option>
+            </select>
+          </label>
+          <label htmlFor="categoriaDespesa">
+            Categoria da Despesa:
+            <select
+              id="categoriaDespesa"
+              name="categoriaDespesa"
+              type="select"
+              data-testid="tag-input"
+              onChange={ this.handleChange }
+            >
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+          <button
+            type="button"
+            onClick={ this.handleButton }
+          >
+            Adicionar despesa
+          </button>
+        </form>
       </div>
     );
   }
